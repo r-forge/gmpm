295b5bd7a254e9fef71ceeeec9d35082cc061014
dalebarr (Dale Barr) <dale.barr@ucr.edu>
2009-08-31 08:29:12 +0000

    added content
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/gmpm@7 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -33,19 +33,31 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 <!-- get project title  -->
 <!-- own website starts here, the following may be changed as you like -->
 
-<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
-$contents = '';
-while (!feof($handle)) {
-	$contents .= fread($handle, 8192);
-}
-fclose($handle);
-echo $contents; } ?>
+<h2>Generalized Multilevel Permutation Models</h2>
+
+<p>The gmpm package provides a comprehensive framework for performing permutation tests using regression on multilevel experimental data.  GMPMs can be used to analyze categorical, count, and continuous data on its natural scale.</p>
+
+#<?php if ($handle=fopen('http://'.$domain.'/export/projtitl.php?group_name='.$group_name,'r')){
+#$contents = '';
+#while (!feof($handle)) {
+#	$contents .= fread($handle, 8192);
+#}
+#fclose($handle);
+#echo $contents; } ?>
 
 <!-- end of project description -->
 
-<p> No content added. </p>
+<p>The package <i>gmpm</i> can be installed from the R command line using the following syntax:</p>
 
-<p> The <strong>project summary page</strong> you can find <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
+<p><strong><code>install.packages("gmpm", repos="http://R-Forge.R-project.org")</code></strong></p>
+
+  <p>To get started with <i>gmpm</i>, please see the package help documentation for the function <i>gmp</i>.  If you are interested in using GMPM to analyze visual-world eyetracking data, please see the example <i>kb07</i> in the package (from the command line in R, type <code>?kb07</code>).</p>
+
+  <p>New features will be added soon.  If you want to receive email updates, please subscribe to the <a href="https://r-forge.r-project.org/news/?group_id=512">'news' mailing list</a>.</p>
+
+  <p>Please see the <a href="https://r-forge.r-project.org/forum/?group_id=512">forums</a> for news/discussion.  If you encounter problems, please submit a report in the "bugs" forum.</p>
+
+<p> You can find the <strong>project summary page</strong> <a href="http://<?php echo $domain; ?>/projects/<?php echo $group_name; ?>/"><strong>here</strong></a>. </p>
 
 </body>
 </html>
